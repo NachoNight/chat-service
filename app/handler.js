fc5a767f1ec4d93efe362121eac0039b9aa847d8
Denis Onder <denis_onder@protonmail.com>
2019-11-02 01:06:59 +0100

    Test client working

--- app/handler.js
+++ app/handler.js
@@ -1,8 +1,26 @@
 const socketIO = require('socket.io');
+const ChatMessage = require('./db/models/chat.model');
 
 module.exports = (server) => {
   const io = socketIO(server);
   io.on('connection', (socket) => {
-    console.log(`Connected: ${socket}`);
+    const getMessages = async (room, global = false) => {
+      const msg = await ChatMessage.findAll({ where: { room } });
+      if (global) {
+        io.sockets.in(room).emit('get_message', msg);
+      } else {
+        socket.emit('get_message', msg);
+      }
+    };
+    socket.on('join_room', ({ name }) => {
+      console.log(`Socket joining room ${name}`);
+      socket.join(name);
+      getMessages(name);
+    });
+    socket.on('send_message', async ({ user, body, room }) => {
+      console.log('New message in room', room);
+      await ChatMessage.create({ user, body, room });
+      getMessages(room, true);
+    });
   });
 };
